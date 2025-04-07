dc320f88dcf8586c4547257bde2e56576490ca32
gelangn <garay.n.a@gmail.com>
2025-04-07 18:09:57 +0200

    Agregar archivos de configuración y funciones para liberar recursos y finalizar el programa

---
+++ src/freeing.c
@@ -0,0 +1,76 @@
+//
+// Created by angel on 6/04/25.
+//
+
+#include "../inc/par_impar.h"
+
+void	free_resources(t_global *global)
+{
+	if (!global)
+		return ;
+	if (global->list_even)
+	{
+		free_list(global->list_even);
+		global->list_even = NULL;
+	}
+	if (global->list_odd)
+	{
+		free_list(global->list_odd);
+		global->list_odd = NULL;
+	}
+	if (global->numbers)
+	{
+		free(global->numbers);
+		global->numbers = NULL;
+	}
+	if (global->threads)
+	{
+		free(global->threads);
+		global->threads = NULL;
+	}
+	if (global->thread_data)
+	{
+		free(global->thread_data);
+		global->thread_data = NULL;
+	}
+	free(global);
+	global = NULL;
+}
+
+void	free_list(t_list *list)
+{
+	t_list	*current;
+	t_list	*next;
+
+	current = list;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+}
+
+void	destroy_mutex(t_global *global)
+{
+	pthread_mutex_destroy(&global->mutex_even);
+	pthread_mutex_destroy(&global->mutex_odd);
+	pthread_mutex_destroy(&global->mutex_count);
+}
+
+void	finish(t_global *global, const char *message)
+{
+	if (errno == 0)
+	{
+		printf("Exiting program: %s\n", message);
+		free_resources(global);
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		perror(message);
+		printf("Exiting program\n");
+		free_resources(global);
+		exit(EXIT_FAILURE);
+	}
+}
